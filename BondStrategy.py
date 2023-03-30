97164d087cd9025e40fb7589b2156eafd586c656
Ubuntu <ubuntu@bot-mantine.nyc-fttp-2023.js-etc>
2023-03-30 15:38:05 +0000

    fix reject order issue

--- BondStrategy.py
+++ BondStrategy.py
@@ -13,6 +13,8 @@ class BondStrategy:
         self.outstanding_sell = 0
         self.order_id = 100000000
 
+        self.order_info = {}
+
         assert(hello_message["type"] == "hello")
         for symbol in hello_message["symbols"]:
             if symbol["symbol"] == "BOND":
@@ -21,6 +23,15 @@ class BondStrategy:
         self.update_orders()
     
     def handle_message(self, message):
+        if message["type"] == "reject":
+            order_id = message["order_id"]
+            if message["error"] == "LIMIT:OPEN_ORDERS" and order_id in self.order_info:
+                if self.order_info[order_id]["type"] == "BUY":
+                    self.outstanding_buy -= self.order_info[order_id]["quantity"]
+                else:
+                    self.outstanding_sell -= self.order_info[order_id]["quantity"]
+                self.update_orders()
+
         if message["type"] == "fill":
             if message["symbol"] != "BOND":
                 return
@@ -48,9 +59,17 @@ class BondStrategy:
     def buy(self, quantity):
         self.exchange.send_add_message(order_id=self.order_id, symbol="BOND", dir=SampleBot.Dir.BUY, price=self.fair_value - self.edge, size=quantity)
         self.outstanding_buy += quantity
+        self.order_info["" + self.order_id] = {
+            "type": "BUY",
+            "amount": quantity
+        }
         self.order_id += 1
     
     def sell(self, quantity):
-        self.outstanding_sell += quantity
         self.exchange.send_add_message(order_id=self.order_id, symbol="BOND", dir=SampleBot.Dir.SELL, price=self.fair_value + self.edge, size=quantity)
+        self.outstanding_sell += quantity
+        self.order_info["" + self.order_id] = {
+            "type": "SELL",
+            "amount": quantity
+        }
         self.order_id += 1
