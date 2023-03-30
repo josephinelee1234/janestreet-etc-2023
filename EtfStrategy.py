9c7801d028dd10078eae7bcb4033ed1b6e2a5298
aschleigh <ashleyzhang04@gmail.com>
2023-03-30 13:17:49 -0400

    m

--- EtfStrategy.py
+++ EtfStrategy.py
@@ -83,7 +83,7 @@ class EtfStrategy:
                 print("ms")
                 print(self.etf)
             
-            self.etf = (3000 + 2*self.gs_fair + 3*self.ms_fair + 3*self.wfc_fair)/10            
+            self.etf = (3000 + 2*self.gs_fair + 3*self.ms_fair + 2*self.wfc_fair)/10            
             
             if message["symbol"] == "XLF":
                 if self.things_added >= 3:
