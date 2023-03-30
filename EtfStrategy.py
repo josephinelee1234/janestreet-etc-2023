cf6468fc9049b9608ae58d1294e4154d36b37415
aschleigh <ashleyzhang04@gmail.com>
2023-03-30 13:54:13 -0400

    m

--- EtfStrategy.py
+++ EtfStrategy.py
@@ -80,27 +80,23 @@ class EtfStrategy:
                     def best_price_etf(side):
                             if message[side]:
                                 return message[side][0][0]
-                    print(message.get("size"))
-                    if self.position <= 90:
-                        if best_price_etf("sell") is not None:
-                            if (best_price_etf("sell") - self.etf) :
-                                print("selling")
-                                self.count += 1
-                                self.position += 0
-
-                                self.exchange.send_add_message(self.count , "XLF", SampleBot.Dir.SELL, best_price_etf("sell"), 10)
-                                self.count += 1
-
+                    print(self.position)
+                    
+                    if best_price_etf("buy") is not None:
+                        while best_price_etf("buy") < self.etf:
+                            if self.position >= -90:
+                                print("buying")
                                 self.exchange.send_add_message(self.count , "XLF", SampleBot.Dir.BUY,  best_price_etf("buy"), 10)
-                                
-                    # elif self.position >= -90:
-                    #     if best_price_etf("buy") is not None:
-                    #         if best_price_etf("buy") < self.etf:
-                    #             print("buying")
-                    #             self.count += 1
-                    #             self.position -= 10
-                                
-                            
+                                self.position -= 10
+                                self.count += 1
+
+                    if best_price_etf("sell") is not None:
+                        while best_price_etf("sell") > self.etf:
+                            if self.position <= 90:
+                                print("selling")
+                                self.exchange.send_add_message(self.count , "XLF", SampleBot.Dir.SELL, best_price_etf("sell"), 10)
+                                self.position += 10
+                                self.count += 1
                 
     
         
